de0996a3fd210e0b14b1571092df2ca524ee3851
zack5 <zack5@users.noreply.github.com>
2025-03-01 23:08:10 -0500

    Small styling and wording tweaks

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -31,7 +31,7 @@ export default function About() {
       </div>
       <h2>Collection</h2>
       
-      <p>In 2023, I began making a concerted effort to collect a transit card from every city I visited. As a public transit enthusiast I love immersing myself in cities with excellent transit networks, though I have also found the challenge of obtaining cards from cities in which transit is less of a priority to be fun as well!</p>
+      <p>In 2023, I began making a concerted effort to collect a transit card from every city I visited. As a public transit enthusiast I love immersing myself in cities with excellent transit networks, though I have also found the challenge of obtaining cards from transit deserts to be fun as well!</p>
 
       <p>As more and more systems allow payment through phone taps or credit cards, the future of transit card collecting is uncertain. While this technology is undeniably more convenient for many users, I'll always have a soft spot for the system-specific cards :)</p>
       
@@ -39,7 +39,7 @@ export default function About() {
       
       <p>From a design perspective, I value clear, distinctive iconography. I think it's especially fun when cards find a unique way to tie into the region they represent. I want a card to be something I enjoy seeing every time I open my wallet. In transit systems, I prize access to a wide range of destinations, ease of use, reliability, and safety.</p>
       
-      <p>Please keep in mind that these ratings are based on my own experiences and opinions! The fact that a region has created a bespoke transit card is itself a testament to its committment to transit. If I have given a system a lower score than you think it deserves, I likely have not had the opportunity to experience it enough.</p>
+      <p>Please keep in mind that these ratings are based on my own experiences and opinions! The fact that a region has created a bespoke transit card is itself a testament to its committment to improving its network. If I have given a system a lower score than you think it deserves, I likely have not had the opportunity to experience it enough.</p>
       
       <h2>Contact</h2>
       <a href="mailto:isaac.cinquini@gmail.com">
